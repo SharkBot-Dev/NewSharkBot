a8d07ede2937a97bf9d58aca1f2682ff59b544ec
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-26 09:51:02 +0900

    feat: ビルドスクリプトのパスをUniQUE-APIからNewSharkBotに変更

--- src/api/scripts/build.sh
+++ src/api/scripts/build.sh
@@ -22,8 +22,8 @@ else
   export VERSION=$(git describe --tags --abbrev=0)
   export GIN_MODE=release
   go build -ldflags "\
-  -X github.com/UniPro-tech/UniQUE-API/internal/config.Version=$VERSION \
-  -X github.com/UniPro-tech/UniQUE-API/internal/config.GitCommit=$COMMIT \
-  -X github.com/UniPro-tech/UniQUE-API/internal/config.GitBranch=$BRANCH" \
+  -X github.com/SharkBot-Dev/NewSharkBot/api/internal.Version=$VERSION \
+  -X github.com/SharkBot-Dev/NewSharkBot/api/internal.GitCommit=$COMMIT \
+  -X github.com/SharkBot-Dev/NewSharkBot/api/internal.GitBranch=$BRANCH" \
   cmd/main.go
 fi
