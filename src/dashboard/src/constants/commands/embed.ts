162799b0bb90894f093298c38ff3bf5821e072a8
sharkbot-neko <modoggu12345@outlook.com>
2026-03-31 20:14:36 +0900

    fix: 埋め込み修正

--- src/dashboard/src/constants/commands/embed.ts
+++ src/dashboard/src/constants/commands/embed.ts
@@ -1,7 +1,10 @@
+import Permissions from "@/constants/Discord/Permissions";
+
 const commands = [
   {
     name: "embed",
-    description: "埋め込みを作成＆保存します。"
+    description: "埋め込みを作成＆保存します。",
+    default_member_permissions: Permissions.ManageGuild.toString(), 
   }
 ];
 
